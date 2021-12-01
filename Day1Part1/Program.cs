cde391754bede98b81b93471c0a673c471bb2e74
Benjamin Rosenthal <brosen8690@gmail.com>
2021-12-01 08:43:12 -0500

    Add project files.

---
+++ Day1Part1/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] inputStrings = File.ReadAllText("../../../input.txt").Split();
+
+int count = 0;
+int previousDepth = int.MaxValue;
+foreach (var depthString in inputStrings.Where(x => !string.IsNullOrEmpty(x)))
+{
+    int intDepth = int.Parse(depthString);
+
+    if (intDepth > previousDepth)
+    {
+        count++;
+    }
+
+    previousDepth = intDepth;
+}
+
+Console.WriteLine(count);
---
+++ Day1Part2/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] depths = File.ReadAllText("../../../input.txt").Split().Where(x => !string.IsNullOrEmpty(x)).Select(x => int.Parse(x)).ToArray();
+
+
+int count = 0;
+int previousCombinedDepth = int.MaxValue;
+
+for (int i = 0; i < depths.Length; i++)
+{
+    if (i < 2)
+    {
+        continue;
+    }
+
+    var currentCombinedDepth = depths[i] + depths[i - 1] + depths[i - 2];
+
+    if (currentCombinedDepth > previousCombinedDepth)
+    {
+        count++;
+    }
+
+    previousCombinedDepth = currentCombinedDepth;
+}
+
+Console.WriteLine(count);
