80e0077fa4f9711154c98844bec31c6a34942590
Benjamin Rosenthal <brosen8690@gmail.com>
2021-12-03 12:01:26 -0500

    clean up

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,18 +1,14 @@
-﻿var rows = File.ReadAllText("../../../input.txt").Split("\n").Where(x => !string.IsNullOrEmpty(x));
+﻿var rows = File.ReadAllText("../../../input.txt").Split("\n").Where(x => !string.IsNullOrEmpty(x)).ToList();
 
 //Part 1
 //Make columns out of the rows
 var columns = new List<string>();
-for (int i = 0; i < rows.ElementAt(i).Length; i++)
+for (int i = 0; i < rows[i].Length; i++)
 {
     columns.Add("");
     for (int j = 0; j < rows.Count(); j++)
     {
-        if (columns.ElementAt(i) == null)
-        {
-            columns.Add("");
-        }
-        columns[i] += rows.ElementAt(j)[i];
+        columns[i] += rows[j][i];
     }
 }
 
@@ -42,6 +38,7 @@ for (int i = 0; i < columns.Count(); i++)
     }
 }
 
+//Bits to int
 int gamma = Convert.ToInt32(gammaBits, 2);
 int epsilon = Convert.ToInt32(epsilonBits, 2);
 
